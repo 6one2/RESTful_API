603fcae3c8fddee6f50c5128ad133f169ca86011
6one2 <sebastien.villard@gmail.com>
2020-01-15 11:14:24 -0500

    Implement authentication for get /item endpoint

--- code/app.py
+++ code/app.py
@@ -1,15 +1,20 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'gdjqoiuj7Gh'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # create endpoint /auth
 
 # here we are not working with database but using python list.
 items = []
 
 #inheritance from the class Resource
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x:x['name']==name, items), None)
         return {'item': item}, 200 if item else 404 # status codes (200:accepted, 404:not found)
