307b96dff7b7257875210e8a64cb1eee7dbbd68d
6one2 <sebastien.villard@gmail.com>
2020-01-14 22:27:58 -0500

    Optimize get method for /item and change messages

--- code/app.py
+++ code/app.py
@@ -10,13 +10,13 @@ items = []
 #inheritance from the class Resource
 class Item(Resource):
     def get(self, name):
-        item = next(filter(lambda x:x['name']==name, items), None)
-        return {'item': item}, 200 if item else 404 # status codes (200:accepted, 404:not found)
+        for item in items:
+            if item['name']==name:
+                return item
+
+        return {'error': f"No item '{name}' was found"}, 404 # status code for not found
 
     def post(self, name):
-        if next(filter(lambda x:x['name']==name, items), None):
-            return {'message': 'An item of name {name} already exists.'}, 400 # bad request
-
         request_data = request.get_json()
         item = {'name': name,
                 'price': request_data['price']}
