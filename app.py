932675debe21be7bbe1a9961b2e127dda6b124db
6one2 <sebastien.villard@gmail.com>
2020-02-05 16:36:39 -0500

    Add jwt configurations messages

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
@@ -32,6 +32,42 @@ def add_claims_to_jwt(identity):
     if identity ==1: # instead of hard-coding, read from config file or database
         return {'is_admin': True}
     return {'is_admin': False}
+
+## configuration / customization of error messages
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({
+        'description':'the token has expired',
+        'error':'token_expired'
+    }), 401
+
+@jwt.invalid_token_loader
+def invalid_token_loader(error):
+    return jsonify({
+        'description': 'Signature verification failed',
+        'error': 'invalid_token'
+    }), 401
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'description': 'Request does not contain an access token',
+        'error': 'authorization_required'
+    })
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback():
+    return jsonify({
+        'description': 'The token is not fresh',
+        'error': 'fresh_token_required'
+    }), 401
+
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return jsonify({
+        'description': 'The token has been revoked',
+        'error': 'token_revoked'
+    }), 401
 
 
 # create the endpoints link to ressources
